aa8c1f1bdd2e521c4a57da99737eaf088969ee77
Yuguo Zhao <cheerzhao77@gmail.com>
2020-11-27 00:58:58 -0600

    update getRacers fuc

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -329,6 +329,10 @@ function getTracks() {
 
 function getRacers() {
 	// GET request to `${SERVER}/api/cars`
+	return fetch(`${SERVER}/api/cars`)
+	.then(response => response.json())
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
 }
 
 function createRace(player_id, track_id) {
